22e7aed698bb7871adfff06f42f9ddffdd0025da
Krzysztof <krzysztof.kacik@sear-polska.pl>
2021-01-04 18:19:02 +0100

    Remove Order module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { typeOrmConfig } from './config';
 import { FlashcardsModule } from './flashcards/flashcards.module';
-import { OrdersModule } from './orders/orders.module';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     FlashcardsModule,
-    OrdersModule,
     TasksModule,
     AuthModule,
   ],
