c9d779390882ac68a5556287f9a7650100fb4ec2
Krzysztof <krzysztof.kacik@sear-polska.pl>
2021-01-04 18:12:29 +0100

    Remove comment

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,3 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 
