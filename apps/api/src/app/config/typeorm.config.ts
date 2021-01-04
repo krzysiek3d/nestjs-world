052220a2e0008da8973facca08d2cb9755dee3b3
Krzysztof <krzysztof.kacik@sear-polska.pl>
2021-01-04 18:02:44 +0100

    change process env vars

--- apps/api/src/app/config/typeorm.config.ts
+++ apps/api/src/app/config/typeorm.config.ts
@@ -6,13 +6,13 @@ const dbConfig = config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: dbConfig.type,
-  host: process.env.KK_HOSTNAME || dbConfig.host,
-  port: process.env.KK_PORT || dbConfig.port,
-  username: process.env.KK_USERNAME || dbConfig.username,
-  password: process.env.KK_PASSWORD || dbConfig.password,
-  database: process.env.KK_DB_NAME || dbConfig.database,
+  host: process.env.DB_HOSTNAME || dbConfig.host,
+  port: process.env.DB_PORT || dbConfig.port,
+  username: process.env.DB_USERNAME || dbConfig.username,
+  password: process.env.DB_PASSWORD || dbConfig.password,
+  database: process.env.DB_NAME || dbConfig.database,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: process.env.KK_TYPEORM_SYNC || dbConfig.synchronize,
+  synchronize: process.env.DB_TYPEORM_SYNC || dbConfig.synchronize,
   autoLoadEntities: true,
   logNotifications: true,
 };
